08b72d5a971bef8830265156caa307ab7c84049b
zsamijon-maker <zsamijon@bisu.edu.ph>
2026-04-15 15:23:09 +0800

    about vercel

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,12 +1,12 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [inspectAttr(), react()],
+  plugins: [react()],
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
